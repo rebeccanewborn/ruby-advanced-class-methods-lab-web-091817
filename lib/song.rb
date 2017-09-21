cc0c8a0d9f4df9b791b67ddf1a85ab72036d80e9
rebeccanewborn <rjn58@cornell.edu>
2017-09-21 19:08:21 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,47 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    Song.new.tap{|song| song.save}
+  end
+
+  def self.new_by_name(name)
+    Song.new.tap do |song|
+      song.name = name
+    end
+  end
+
+  def self.create_by_name(name)
+    self.new_by_name(name).tap {|song| song.save}
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|song| song.name==name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    arr = filename.split(" - ")
+    song = Song.new_by_name(arr[1].chomp(".mp3"))
+    song.artist_name = arr[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename).tap {|song| song.save}
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+
+
 end
